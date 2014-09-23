6eb0c3af261bd821a4504f791816a5495377fd09
Josh Hall <josh@yaplabs.com>
2014-09-23 11:41:42 -0700

    Expand scanner to account for non-bmml files

--- app/scanner.rb
+++ app/scanner.rb
@@ -25,25 +25,92 @@ class Scanner
     @index = Index.new
   end
   
-
-  # Testing pieces
+  
+  # TODO Add function to also scan for files that have been removed since the last index
+  # Scan all projects to update anything new
   def all
-    # Change working directory to accountRoot
+    # Log beginning of index process
+    puts @log.info "Begin indexing all projects"
+    
+    # Variables to store what will need processing after running through the index
+    component = []
+    project = []
+    file = []
+    
+    # Walk through project assets
     Dir.chdir @settings['accountRoot'] do
-      # Log beginning of complete export
-      puts @log.info "Scanning all projects"
       
-      # Locate all BMML files to export
-      bmmlFiles = File.join("**", "*.bmml")
-      
-      # Walk through all available BMML files
-      Dir.glob(bmmlFiles).each do |f|
-        # Ignore .bmml files in the asset directory
-        unless f =~ /\/Wireframes.*\/assets\//
-          # Export all of the bmml files found
-          @export.file f
+      # Walk through all projects
+      Dir.glob("**/*").each do |f|
+        # Set the absolute path
+        f = File.absolute_path f
+        
+        # Refresh @index
+        @index.refresh
+        
+        # Make sure this is a valid file type,
+        # is not in the /Screens directory, and
+        # has changed since the last index
+        if @settings['fileTypes'].include?((File.extname f).downcase) and \
+          not File.dirname(f).end_with?('Screens') and \
+          @index.updated? f
+          
+          # Capture this if it's a component
+          if File.dirname(f) == @settings['componentsProject']
+            # Add the file to the component list
+            component.push File.absolute_path(f)
+            
+          # Capture this if it's a project asset
+          elsif File.dirname(f).end_with?('assets')
+            # Add the project to the project list
+            path = f
+            path.slice! @settings['accountRoot'] + '/'
+            path = path.split('/')[0]
+        
+            # Find just the first directory in the path, this is the project dir
+            project.push File.join(@settings['accountRoot'], path)
+            
+          # Check for individual files
+          elsif not File.dirname(f) == @settings['componentsProject'] and \
+            not File.dirname(f).end_with?('assets') and (\
+            File.extname f).downcase == ".bmml"
+            
+            # include the file
+            file.push File.absolute_path(f)
+            
+          end
         end
       end
     end
+    
+    # Cleanup the duplicates (if any)
+    component.uniq!
+    project.uniq!
+    file.uniq!
+    
+    
+    ### Export stale files ###
+    # If a component is updated, export everything
+    unless component.empty?
+      @export.all
+    else
+      
+      # If a project is updated, export each project first
+      unless project.empty?
+        project.each do |p|
+          @export.project p
+        end
+        
+      # Export individual files remaining
+      # TODO create exception for the projects that have been exported
+      else
+        file.each do |n|
+          @export.file n
+        end
+      end
+    end
+    
+    # Log end of index process
+    puts @log.info "Done indexing all projects"
   end
 end
